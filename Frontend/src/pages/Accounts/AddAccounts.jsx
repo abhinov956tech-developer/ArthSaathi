3244a5d8bb198718b68033c8f1e9584f668e1fe4
Deepmoina Boruah <deepmoinaboruah7@gmail.com>
2025-03-08 10:51:14 +0530

    Delete Frontend directory

--- Frontend/src/pages/Accounts/AddAccounts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddAccounts = () => {
-  return (
-    <div>AddAccounts</div>
-  )
-}
-
-export default AddAccounts
--- Frontend/src/pages/Accounts/AllAccounts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AllAccounts = () => {
-  return (
-    <div>AllAccounts</div>
-  )
-}
-
-export default AllAccounts
